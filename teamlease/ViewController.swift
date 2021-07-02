6b76092728abd9082f783d4025747e8419733bc5
manitha <manitha.samarsan93@gmail.com>
2021-07-02 10:25:40 +0530

    Initial Commit

---
+++ teamlease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamlease
+//
+//  Created by Pandoo on 02/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
